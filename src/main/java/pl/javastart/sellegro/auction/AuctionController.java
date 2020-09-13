ed2efb40ba0b8fad72b6e99d58d02b48a071c69d
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-09-13 17:58:53 +0200

    Connection to database functions and sorting also. Filtering does not function.

--- src/main/java/pl/javastart/sellegro/auction/AuctionController.java
+++ src/main/java/pl/javastart/sellegro/auction/AuctionController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.javastart.sellegro.repository.AuctionsRepository;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -67,5 +68,48 @@ public class AuctionController {
         }
         model.addAttribute("auctions", auctionsList);
         return "auctions";
+    }
+    @GetMapping("/filters")
+    public String filters (Model model,
+                           @RequestParam(required = false, defaultValue = "ALL") Filters filter,
+                           @RequestParam(required = false) Long id,
+                           @RequestParam(required = false) String carMake,
+                           @RequestParam(required = false) String carModel,
+                           @RequestParam(required = false) String color,
+                           @RequestParam(required = false) BigDecimal price,
+                           @RequestParam(required = false)LocalDate endDate){
+
+        List<Auctions> filteredList;
+        switch (filter) {
+            case ID:
+                filteredList = auctionsRepository.findByIdStartingWith(id);
+                break;
+            case CAR_MAKE:
+                filteredList = auctionsRepository.findByCarMakeStartingWith(carMake);
+                break;
+            case CAR_MODEL:
+                filteredList = auctionsRepository.findByCarModelStartingWith(carModel);
+                break;
+            case COLOR:
+                filteredList = auctionsRepository.findByColorStartingWith(color);
+                break;
+            case PRICE:
+                filteredList = auctionsRepository.findByPriceStartingWith(price);
+                break;
+            case END_DATE:
+                filteredList = auctionsRepository.findByEndDateContaining(endDate);
+                break;
+            default:
+                filteredList = auctionsRepository.findAll();
+                break;
+        }
+        model.addAttribute("id", id);
+        model.addAttribute("carMake", carMake);
+        model.addAttribute("carModel", carModel);
+        model.addAttribute("color", color);
+        model.addAttribute("price", price);
+        model.addAttribute("endDate", endDate);
+        model.addAttribute("filtered", filteredList);
+        return "filters";
     }
 }
--- src/main/java/pl/javastart/sellegro/repository/AuctionsRepository.java
+++ src/main/java/pl/javastart/sellegro/repository/AuctionsRepository.java
@@ -3,6 +3,8 @@ package pl.javastart.sellegro.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.javastart.sellegro.auction.Auctions;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AuctionsRepository extends JpaRepository<Auctions, Long> {
@@ -31,4 +33,15 @@ public interface AuctionsRepository extends JpaRepository<Auctions, Long> {
 
     List<Auctions> findByOrderByEndDateDesc();
 
+    List<Auctions> findByIdStartingWith(Long id);
+
+    List<Auctions> findByCarMakeStartingWith(String propriety);
+
+    List<Auctions> findByCarModelStartingWith(String propriety);
+
+    List<Auctions> findByColorStartingWith(String propriety);
+
+    List<Auctions> findByPriceStartingWith(BigDecimal price);
+
+    List<Auctions> findByEndDateContaining(LocalDate endDate);
 }
